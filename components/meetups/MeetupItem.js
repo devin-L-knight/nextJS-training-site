24405b480ec6fc8977a762ad7991d8678a2642cd
Devin Knight <devin.knight@accenture.com>
2022-08-09 12:14:15 -0400

    created click handler for the 'Show Details' button using useRouter hook from nextJS & programmatically pushed a new dynamic id in the URL using .push method. Also created a rough singleMeetupDetails page to build out.

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,17 @@
+import { useRouter } from 'next/router'; // this is our nextJS hook for routing
+
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
+  const nextJSrouter = useRouter();
+
+  function showDetailsHandler(evt) {
+    evt.preventDefault();
+    console.log('CLICKED!')
+    nextJSrouter.push(`/${props.id}`);
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +23,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
---
+++ pages/[meetupID]/index.js
@@ -0,0 +1,11 @@
+
+
+function singleMeetUpDetails() {
+    return (
+        <div>
+            ===== TESTING SINGLE MEETUP PAGE =====
+        </div>
+    )
+}
+
+export default singleMeetUpDetails;
