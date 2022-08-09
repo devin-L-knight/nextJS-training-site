769cee6162d895ff1c4011837ea0f8995464c12a
Devin Knight <devin.knight@accenture.com>
2022-08-09 11:55:56 -0400

    Added comment to _app.js for context.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 
+// this is our root component that nextJS will render.
+// the <Component> tag below will become our actual page content of our different pages (this changes when navigating from page A to page B, etc.)
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
