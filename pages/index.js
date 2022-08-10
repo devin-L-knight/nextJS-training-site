0bcf3d0841a9b364ecea39a016c8adcb0b539d5a
Devin Knight <devin.knight@accenture.com>
2022-08-10 11:37:46 -0400

    added getStaticProps & experimented with getServerSideProps in the HomePage component with comments providing additional context about the difference between the two.

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,41 @@ const DUMMY_MEETUPS = [
 ]
 
 
-function HomePage() {
+function HomePage(props) {
+    console.log('PROPS >>>>> ',props)
     return (
-        <MeetupList meetups={DUMMY_MEETUPS} />
+        <MeetupList meetups={props.meetups} />
     )
+}
+
+
+// // This is an ALTERNATE async function to the getStaticProps func below. This is guaranteed to run for every request (but supports multiple changes to the page and any required site authentication, etc.)
+// export async function getServerSideProps(context) {
+//     const request = context.req;
+//     const response = context.res;
+
+//     // similar to getStaticProps, this function MUST always return an object that has a nested props object.
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         }
+//     };
+// }
+
+
+// export nextJS functions from the home page to use elsewhere. This function (getStaticProps) is good to use when data doesn't change all the time (like multiple times a second) and when don't need authentication, this is better.
+// the getStaticProps can run code that would usually run in the server (because the code that is executed here is not on the client and is ONLY run during the initial build process). This is a faster option to get props compared to the getServerSideProps above.
+export async function getStaticProps() {
+    // fetch data from an API
+
+    // this function MUST always return an object, which must have a nested props object.
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        },
+        // revalidate is for "incremental static generation". This property wants a number that represents the number of seconds nextJS will wait before regenerating if requests come in for the page
+        revalidate: 5
+    };
 }
 
 export default HomePage;
